361208247c8f9ae907c403c159bc50d6b1847519
Bruno-Martin-beep <brunico040@gmail.com>
2022-08-23 15:45:45 -0300

    First commit

---
+++ src/App/navbar.tsx
@@ -0,0 +1,21 @@
+const navbar = () => {
+  return (
+    <div>
+      <h1>logo</h1>
+      <ul>
+        <li>Movies</li>
+        <li>TV shows</li>
+        <li>Animations</li>
+        <li>Plans</li>
+      </ul>
+      <div>
+        <div>search</div>
+        <div>circle</div>
+        <div>4 circle</div>
+        <div>user</div>
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
