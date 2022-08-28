47dc89d872583a58e80ea212a2dc3103180a1787
Bruno-Martin-beep <brunico040@gmail.com>
2022-08-28 14:23:36 -0300

    Add grid tablet style

--- src/App/components/PopularMoviesList.tsx
+++ src/App/components/PopularMoviesList.tsx
@@ -28,19 +28,19 @@ const PopularMoviesList = () => {
   };
 
   return (
-    <div className="w-full h-[calc(100vh-40rem)] sm:h-[calc(100vh-6rem)] mb-80 sm:mb-0 mt-80 sm:mt-24  px-[6rem] overflow-auto">
-      <h2 className="mt-24 sm:mt-8 ml-24 sm:ml-0 text-7xl sm:text-xl font-bold">
+    <div className="w-full h-[calc(100vh-40rem)] sm:h-[calc(100vh-20rem)] lg:h-[calc(100vh-6rem)] mt-80 sm:mt-40 lg:mt-24 mb-80 sm:mb-40 lg:mb-0 px-[6rem] overflow-auto">
+      <h2 className="mt-24 sm:mt-16 lg:mt-8 ml-24 sm:ml-0 text-7xl sm:text-5xl lg:text-xl font-bold">
         Popular movies
       </h2>
       {popularList.length !== 0 && (
-        <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-20 sm:gap-8 w-full mt-24 sm:mt-8">
+        <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-20 sm:gap-16 lg:gap-8 w-full mt-24 sm:mt-16 lg:mt-8">
           {popularList.map((movie) => {
             return <DefaultFilm key={movie.id} movie={movie} />;
           })}
         </div>
       )}
       <Waypoint onEnter={handleOnscroll} />
-      <div className="w-full h-80 sm:h-20" />
+      <div className="w-full h-80 sm:h-48 lg:h-20" />
     </div>
   );
 };
